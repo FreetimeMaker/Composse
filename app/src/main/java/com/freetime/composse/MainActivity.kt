cccd47a87c62ee74b3d81c2ae84c057608c636e2
Freetime Maker <jamieachatz@gmail.com>
2026-01-28 22:02:19 +0100

    Changed

--- app/src/main/java/com/freetime/composse/MainActivity.kt
+++ app/src/main/java/com/freetime/composse/MainActivity.kt
@@ -3,36 +3,12 @@ package com.freetime.composse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.freetime.composse.ui.theme.ComposseTheme
-
+import com.freetime.composse.ui.screens.MapScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ComposseTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    SimpleMapApp()
-                }
-            }
+            MapScreen()
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SimpleMapAppPreview() {
-    ComposseTheme {
-        SimpleMapApp()
     }
 }
